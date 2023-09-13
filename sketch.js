97fadf6292504121b7d274ce67d16da2c4eb51bb
Alisha Galarza <aag19g@fsu.edu>
2023-09-13 10:30:13 -0400

    Creating Variables

--- sketch.js
+++ sketch.js
@@ -1,52 +1,17 @@
+let xPosition
+
 function setup() {
   createCanvas(400, 400);
-  noLoop()
-  strokeWeight(10)
+  xPosition = width/2;
+  console.log(xPosition);
+  noStroke()
+
 }
 
 function draw() {
-  background("#FF5733");
-  
-  fill("white")
-  push()
-  rect(4, 5, 196, 300)
-  pop()
-
-fill("#FFC300")
-rect(5, 5, 125, 230)
-
-fill("white")
-push()
-translate(width/2, height/2)
-rect(0, 0, 195, 196)
-pop()
-
-fill("#FF5733")
-push()
-strokeWeight(5)
-translate(width/2, height/2)
-rect(40, 2, 50, 30)
-pop()
-
-fill("#C70039")
-push()
-strokeWeight(5)
-translate(width/2, height/2)
-rect(40, 40, 50, 50)
-pop()
-
-fill("#900C3F")
-push()
-strokeWeight(5)
-translate(width/2, height/2)
-rect(40, 100, 50, 50)
-pop()
-
-fill("#581845")
-push()
-strokeWeight(5)
-translate(width/2, height/2)
-rect(40, 160, 50,24)
-pop()
+  background("#85BDBF");
+  fill("#C2FCF7")
+  ellipse(xPosition, height/2, 40);
+  xPosition = xPosition  + .1
 
 }

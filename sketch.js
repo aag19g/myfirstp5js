bba1bcf1b04848bf01eeecd28beb827b5dc17572
Alisha Galarza <aag19g@fsu.edu>
2023-09-13 10:36:49 -0400

    creating variables.2

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
-let xPosition
+let x = 0;
+let speed = .2;
 
 function setup() {
   createCanvas(400, 400);
-  xPosition = width/2;
-  console.log(xPosition);
   noStroke()
-
 }
 
 function draw() {
   background("#85BDBF");
+
   fill("#C2FCF7")
-  ellipse(xPosition, height/2, 40);
-  xPosition = xPosition  + .1
+  ellipse(x, height/2, 40);
+  // x+= speed;
+  x = x + speed;
 
 }

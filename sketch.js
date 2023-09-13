1a58129c12b5a812a3862664687f6c5d7abd5856
Alisha Galarza <aag19g@fsu.edu>
2023-09-12 21:02:43 -0400

    Done with part 1 of this project - Edited

--- sketch.js
+++ sketch.js
@@ -1,48 +1,51 @@
 function setup() {
   createCanvas(400, 400);
   noLoop()
-  noStroke()
+  strokeWeight(10)
 }
 
 function draw() {
   background("#FF5733");
-
-  fill("black")
+  
+  fill("white")
   push()
-  rect(0, 0, 200, 300)
+  rect(4, 5, 196, 300)
   pop()
 
 fill("#FFC300")
-rect(0, 0, 125, 230)
+rect(5, 5, 125, 230)
 
-fill("black")
+fill("white")
 push()
 translate(width/2, height/2)
-rect(0, 0, 200, 400)
+rect(0, 0, 195, 196)
 pop()
 
 fill("#FF5733")
 push()
+strokeWeight(5)
 translate(width/2, height/2)
-rect(40, 0, 50, 30)
+rect(40, 2, 50, 30)
 pop()
 
 fill("#C70039")
 push()
+strokeWeight(5)
 translate(width/2, height/2)
 rect(40, 40, 50, 50)
 pop()
 
 fill("#900C3F")
 push()
+strokeWeight(5)
 translate(width/2, height/2)
 rect(40, 100, 50, 50)
 pop()
 
 fill("#581845")
 push()
+strokeWeight(5)
 translate(width/2, height/2)
-rect(40, 160, 50, 50)
+rect(40, 160, 50,24)
 pop()
-
 }

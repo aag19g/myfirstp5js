5042e4ce85e05038b1f555aab11e8cc358f54e95
Alisha Galarza <aag19g@fsu.edu>
2023-09-12 20:26:12 -0400

    In progress

--- sketch.js
+++ sketch.js
@@ -5,18 +5,44 @@ function setup() {
 }
 
 function draw() {
-  background("teal");
+  background("#FF5733");
 
   fill("black")
   push()
   rect(0, 0, 200, 300)
   pop()
 
-fill("teal")
+fill("#FFC300")
 rect(0, 0, 125, 230)
 
+fill("black")
+push()
+translate(width/2, height/2)
+rect(0, 0, 200, 400)
+pop()
 
+fill("#FF5733")
+push()
+translate(width/2, height/2)
+rect(40, 0, 50, 30)
+pop()
 
+fill("#C70039")
+push()
+translate(width/2, height/2)
+rect(40, 40, 50, 50)
+pop()
 
+fill("#900C3F")
+push()
+translate(width/2, height/2)
+rect(40, 100, 50, 50)
+pop()
+
+fill("#581845")
+push()
+translate(width/2, height/2)
+rect(40, 160, 50, 50)
+pop()
 
 }

572711293207dbf7aec7cb262ad07ad638f41d19
Alisha Galarza <aag19g@fsu.edu>
2023-09-20 09:00:09 -0400

    ball changes different color of gray when clicked

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let ballX = 0;
 let rectX = 0;
 let ballSpeed = 15;
-let shouldMove = true
+let moving = true
+let gray = 124
 
 function setup() {
   createCanvas(400, 400);
@@ -14,15 +15,24 @@ function draw() {
     if (ballX > width || ballX < 0) {
       ballSpeed = ballSpeed * -1;
     }
-    if(shouldMove){
+    if(moving){
     ballX = ballX + ballSpeed;
     }
+    fill(gray);
     ellipse(ballX, height/2, 30)
 
+    fill(255);
     rectX = rectX + 1;
     rect(ballX, 100, 30, 30);
   }
 
     function mouseClicked(){
-      shouldMove = ! shouldMove;
+      if(moving){
+        moving = false;
+        gray = random(255);
+      } else{
+        moving = true
+      }
+      moving = ! moving;
+      gray = random(255);
     }

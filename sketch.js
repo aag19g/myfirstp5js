ea4cc66c778b9975643a8e1663c944dc318d643f
Alisha Galarza <aag19g@fsu.edu>
2023-09-20 08:35:33 -0400

    start and stop with Ifesle function

--- sketch.js
+++ sketch.js
@@ -1,31 +1,47 @@
 let x = 200;
 let speed = 4;
 let diameter = 40;
-// let stop
+let stop
 let start
+let moving = true
 
 function setup() {
   createCanvas(400, 400);
   noStroke()
-  noLoop()
+  // noLoop()
 }
 
 // function mousePressed(){
 // stop = noLoop()
-// }
+// } 
 
 function draw() {
   background("#85BDBF");
 
   fill("#C2FCF7");
   ellipse(x, height/2, diameter);
-  //if (x <= width - diameter/2) {
     if (x >= width - diameter/2 || x<= diameter/2) {
       speed = speed * -1;
     }
-    x = x + speed;
-}
+    x = x + speed;}
 
-function mousePressed(){
-  start = loop()
-}
+    function mouseClicked(){
+      if (moving == true){
+        noLoop();
+        moving = false;
+      }else {
+        loop();
+        moving = true;
+      }
+    }
+
+  // fill("#C2FCF7");
+  // rect(x, height/2, diameter);
+  //   if (x >= width - diameter/2 || x<= diameter/2) {
+  //     speed = speed * -2;
+  //   }
+  //   x = x + speed;}
+    
+// function mousePressed(){
+// start = loop()
+// }
